25586a893af3ccaa72520e9d134ed870e19d25b4
Hao Ly <kevinly0609@gmail.com>
2024-07-26 13:38:42 -0700

    git ignore

--- node_modules/next/dist/client/components/static-generation-bailout.d.ts
+++
@@ -1,8 +0,0 @@
-import type { AppConfigDynamic } from '../../build/utils';
-type BailoutOpts = {
-    dynamic?: AppConfigDynamic;
-    link?: string;
-};
-export type StaticGenerationBailout = (reason: string, opts?: BailoutOpts) => boolean | never;
-export declare const staticGenerationBailout: StaticGenerationBailout;
-export {};
